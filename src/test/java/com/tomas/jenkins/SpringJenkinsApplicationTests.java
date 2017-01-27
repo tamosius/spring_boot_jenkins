1bbcce9c84367db31b497cbb0391976f18c3b385
tomas <t.mikoliunas@gmail.com>
2017-01-27 23:42:06 +0000

    starting project

---
+++ src/test/java/com/tomas/jenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tomas.jenkins;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJenkinsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
