cfdf6cb8dfaa0dc9010a29e9c1200c5f95bef710
tomas <t.mikoliunas@gmail.com>
2017-01-28 17:03:32 +0000

    made a mistake

---
+++ src/main/java/com/tomas/model/Admin.java
@@ -0,0 +1,19 @@
+package com.tomas.model;
+
+public class Admin {
+	
+	private String firstName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public void setAgain(double firstName){
+		
+		this.firstName = firstName;
+	}
+}
